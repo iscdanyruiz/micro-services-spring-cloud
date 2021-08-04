925bc6a4352807d4c24b66e04e23ba6ee7a35fe1
isc.daniel.ruiz <isc.daniel.ruiz@gmail.com>
2021-08-04 10:35:11 -0500

    Consumiendo API REST con RestTemplate desde servicio Items

--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/model/service/ItemServiceImpl.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/model/service/ItemServiceImpl.java
@@ -7,6 +7,9 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -24,7 +27,7 @@ public class ItemServiceImpl implements ItemService {
 		// List<Producto> productos = Arrays
 		// .asList(clientRest.getForObject("http://localhost:8001/listar",
 		// Producto[].class));
-		
+
 		List<Producto> productos = Arrays
 				.asList(clientRest.getForObject("http://servicio-productos/listar", Producto[].class));
 		return productos.stream().map(p -> new Item(p, 1)).collect(Collectors.toList());
@@ -34,9 +37,37 @@ public class ItemServiceImpl implements ItemService {
 	public Item findById(Long id, Integer cantidad) {
 		Map<String, String> pathVariables = new HashMap<String, String>();
 		pathVariables.put("id", id.toString());
-		//Producto producto = clientRest.getForObject("http://localhost:8001/ver/{id}", Producto.class, pathVariables);
-		Producto producto = clientRest.getForObject("http://servicio-productos/ver/{id}", Producto.class, pathVariables);
+		// Producto producto = clientRest.getForObject("http://localhost:8001/ver/{id}",
+		// Producto.class, pathVariables);
+		Producto producto = clientRest.getForObject("http://servicio-productos/ver/{id}", Producto.class,
+				pathVariables);
 		return new Item(producto, cantidad);
+	}
+
+	@Override
+	public Producto save(Producto producto) {
+		HttpEntity<Producto> body = new HttpEntity<Producto>(producto);
+		ResponseEntity<Producto> response = clientRest.exchange("http://servicio-productos/crear", HttpMethod.POST,
+				body, Producto.class);
+		Producto productoResponse = response.getBody();
+		return productoResponse;
+	}
+
+	@Override
+	public Producto update(Producto producto, Long id) {
+		Map<String, String> pathVariables = new HashMap<String, String>();
+		pathVariables.put("id", id.toString());
+		HttpEntity<Producto> body = new HttpEntity<Producto>(producto);
+		ResponseEntity<Producto> response = clientRest.exchange("http://servicio-productos/editar/{id}", HttpMethod.PUT,
+				body, Producto.class, pathVariables);
+		return response.getBody();
+	}
+
+	@Override
+	public void delete(Long id) {
+		Map<String, String> pathVariables = new HashMap<String, String>();
+		pathVariables.put("id", id.toString());
+		clientRest.delete("http://servicio-productos/eliminar/{id}", pathVariables);
 	}
 
 }
