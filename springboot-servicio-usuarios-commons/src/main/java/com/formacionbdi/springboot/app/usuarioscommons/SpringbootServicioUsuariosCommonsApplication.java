28d2e69190de3a9c7482e4b6615fa2804cd871d9
isc.daniel.ruiz <isc.daniel.ruiz@gmail.com>
2021-08-10 13:11:29 -0500

    Creando librería commons de usuarios

---
+++ springboot-servicio-usuarios-commons/src/main/java/com/formacionbdi/springboot/app/usuarioscommons/SpringbootServicioUsuariosCommonsApplication.java
@@ -0,0 +1,16 @@
+package com.formacionbdi.springboot.app.usuarioscommons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
+public class SpringbootServicioUsuariosCommonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServicioUsuariosCommonsApplication.class, args);
+	}
+
+}
