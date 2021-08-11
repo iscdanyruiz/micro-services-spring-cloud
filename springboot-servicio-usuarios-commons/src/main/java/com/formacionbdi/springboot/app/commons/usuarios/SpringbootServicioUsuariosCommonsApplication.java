b5a28ccdaffabfe24d38a4ba0e53bc18cf684ddd
isc.daniel.ruiz <isc.daniel.ruiz@gmail.com>
2021-08-10 21:37:18 -0500

    Corregida dependencia hacia usuarios commons

--- springboot-servicio-usuarios-commons/src/main/java/com/formacionbdi/springboot/app/commons/usuarios/SpringbootServicioUsuariosCommonsApplication.java
+++ springboot-servicio-usuarios-commons/src/main/java/com/formacionbdi/springboot/app/commons/usuarios/SpringbootServicioUsuariosCommonsApplication.java
@@ -10,7 +10,6 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 public class SpringbootServicioUsuariosCommonsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootServicioUsuariosCommonsApplication.class, args);
 	}
 
 }
--- springboot-servicio-usuarios/src/main/java/com/formacionbdi/springboot/app/usuarios/SpringbootServicioUsuariosApplication.java
+++ springboot-servicio-usuarios/src/main/java/com/formacionbdi/springboot/app/usuarios/SpringbootServicioUsuariosApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@EntityScan({".app.commons.usuarios.models.entity"})
+@EntityScan({"com.formacionbdi.springboot.app.commons.usuarios.models.entity"})
 @SpringBootApplication
 public class SpringbootServicioUsuariosApplication {
 
